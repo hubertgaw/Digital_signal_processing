233b94102fdbbb9a44de9d9799ea09a8ffdedf80
d-jakubik <224307@edu.p.lodz.pl>
2021-03-25 00:36:38 +0100

    Added parameters for signal dialog set

--- cps_project/src/main/java/pl/cps/App.java
+++ cps_project/src/main/java/pl/cps/App.java
@@ -5,6 +5,8 @@ import javafx.application.Preloader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pl.cps.signal.emiters.*;
 import pl.cps.view.MainLayout;
@@ -15,6 +17,7 @@ public class App extends Application {
 
     private String windowTitle;
 
+    Double ampValue, strTimeValue, durValue, termValue, freqValue, possValue, kwValue, jumpValue;
 
     private MainLayout mainLayout = new MainLayout();
 
@@ -41,7 +44,7 @@ public class App extends Application {
         mainLayout.add(calculateButton, 1, 1);
         mainLayout.add(nextButton, 1, 2);
         calculateButton.setOnMouseClicked((action) -> {
-            startCalculating();
+            startCalculating(stage);
         });
         nextButton.setOnMouseClicked((action) -> {
             loadNextDiagram();
@@ -75,16 +78,14 @@ public class App extends Application {
         mainLayout.add(vBox, 0, 0);
     }
 
-    private void loadNextDiagram(){
+    private void loadNextDiagram() {
         System.out.println("NEXT");
     }
 
-    private void startCalculating() {
+    private void startCalculating(Stage stage) {
         System.out.println("OBLICZAM");
-        System.out.println(getSellections()[0]);
-        System.out.println(getSellections()[1]);
-        System.out.println(getSellections()[2]);
-        System.out.println("=============");
+        getSellections(stage);
+
     }
 
     private void addItemsToMenu(String[] items, Menu menu) {
@@ -102,9 +103,9 @@ public class App extends Application {
         }
     }
 
-    public Object[] getSellections() {
-        Object[] ret = {getSignalFromMenuItemName(getSellectdOptionFromMenu(signalOneMenu)),
-                getSignalFromMenuItemName(getSellectdOptionFromMenu(signalTwoMenu)),
+    public Object[] getSellections(Stage stage) {
+        Object[] ret = {getSignalFromMenuItemName(getSellectdOptionFromMenu(signalOneMenu), stage),
+                getSignalFromMenuItemName(getSellectdOptionFromMenu(signalTwoMenu), stage),
                 getSellectdOptionFromMenu(operationMenu)};
         return ret;
     }
@@ -120,31 +121,101 @@ public class App extends Application {
         return ret;
     }
 
-    private Signal getSignalFromMenuItemName(String name) {
+    private Signal getSignalFromMenuItemName(String name, Stage stage) {
         Signal ret = null;
+        setParametersDialogShow(stage);
         if (name == "Szum Gaussowski")
-            ret = new GaussianNoise(1, 1, 1);
+            ret = new GaussianNoise(ampValue, strTimeValue, durValue);
         if (name == "Szum Impulsowy")
-            ret = new ImpulseNoise(1, 1, 1, 1, 1);
+            ret = new ImpulseNoise(ampValue, strTimeValue, durValue, freqValue, possValue);
         if (name == "Sygnał sinusoidalny wyprostowany jednopolowkowo")
-            ret = new OneHalfSinusoidalSignal(1, 1, 1, 1);
+            ret = new OneHalfSinusoidalSignal(ampValue, strTimeValue, durValue, termValue);
         if (name == "Sygnał sinusoidalny wyprostowany dwopolowkowo")
-            ret = new TwoHalfSinusoidalSignal(1, 1, 1, 1);
+            ret = new TwoHalfSinusoidalSignal(ampValue, strTimeValue, durValue, termValue);
         if (name == "Sygnal sinusoidalny")
-            ret = new SinusoidalSignal(1, 1, 1, 1);
+            ret = new SinusoidalSignal(ampValue, strTimeValue, durValue, termValue);
         if (name == "Sygnal prostokatny")
-            ret = new SquareSignal(1, 1, 1, 1, 1);
+            ret = new SquareSignal(ampValue, strTimeValue, durValue, termValue, kwValue);
         if (name == "Sygnal prostokatny symetryczny")
-            ret = new SymmetricalSquareSignal(1, 1, 1, 1, 1);
+            ret = new SymmetricalSquareSignal(ampValue, strTimeValue, durValue, freqValue, kwValue);
         if (name == "Sygnal trojkatny")
-            ret = new TriangularSignal(1, 1, 1, 1, 1);
+            ret = new TriangularSignal(ampValue, strTimeValue, durValue, termValue, kwValue);
         if (name == "Szum o rozkladzie jednostajnym")
-            ret = new UniformlyDistributedNoise(1, 1, 1);
+            ret = new UniformlyDistributedNoise(ampValue, strTimeValue, durValue);
         if (name == "Impuls jednostkowy")
-            ret = new UnitImpulse(1, 1, 1, 1, 1);
+            ret = new UnitImpulse(ampValue, strTimeValue, durValue, freqValue, jumpValue.intValue());
         if (name == "Skok jednostkowy")
-            ret = new UnitJump(1, 1, 1, 1);
+            ret = new UnitJump(ampValue, strTimeValue, durValue, jumpValue);
         return ret;
+    }
+
+    private void setParametersDialogShow(Stage stage) {
+        Stage dialog = new Stage();
+        Button saveBtn = new Button("Dalej");
+        VBox ampBox = new VBox(), strtBox = new VBox(), durBox = new VBox(), termBox = new VBox(),
+                freqBox = new VBox(), possBox = new VBox(), kwBox = new VBox(), jumpBox = new VBox();
+        TextField amp = new TextField("0"), strTime = new TextField("0"), dur = new TextField("0"),
+                term = new TextField("0"), freq = new TextField("0"), poss = new TextField("0"), kw = new TextField("0"), jump = new TextField("0");
+        Text ampText = new Text("Amplitude:"), strTimeText = new Text("Start w sekundzie:"),
+                durText = new Text("Czas trwania w sekundach"),
+                termText = new Text("term:"), freqText = new Text("czestotliwosc:"), possText = new Text("Prawdopodobienstwo:"),
+                kwText = new Text("wspolczynnik wypelnienia"), jumpText = new Text("Informacje odnośnie skoku");
+
+        ampBox.getChildren().add(ampText);
+        ampBox.getChildren().add(amp);
+        strtBox.getChildren().add(strTimeText);
+        strtBox.getChildren().add(strTime);
+        durBox.getChildren().add(durText);
+        durBox.getChildren().add(dur);
+        termBox.getChildren().add(termText);
+        termBox.getChildren().add(term);
+        freqBox.getChildren().add(freqText);
+        freqBox.getChildren().add(freq);
+        possBox.getChildren().add(possText);
+        possBox.getChildren().add(poss);
+        kwBox.getChildren().add(kwText);
+        kwBox.getChildren().add(kw);
+        jumpBox.getChildren().add(jumpText);
+        jumpBox.getChildren().add(jump);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(stage);
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.getChildren().add(ampBox);
+        dialogVbox.getChildren().add(strtBox);
+        dialogVbox.getChildren().add(durBox);
+        dialogVbox.getChildren().add(termBox);
+        dialogVbox.getChildren().add(freqBox);
+        dialogVbox.getChildren().add(possBox);
+        dialogVbox.getChildren().add(kwBox);
+        dialogVbox.getChildren().add(jumpBox);
+        dialogVbox.getChildren().add(saveBtn);
+        Scene dialogScene = new Scene(dialogVbox, 600, 800);
+        dialog.setScene(dialogScene);
+        dialog.show();
+        saveBtn.setOnMouseClicked((action) -> {
+            reloadValues(amp, strTime, dur, term, freq, poss, kw, jump);
+            dialog.getScene().getWindow().hide();
+        });
+
+    }
+
+    private void reloadValues(TextField amp, TextField strTime, TextField dur, TextField term, TextField freq,
+                              TextField poss, TextField kw, TextField jump) {
+        System.out.println("AMP:"+ampValue);
+        ampValue = convertStringToDouble(amp.getText());
+        strTimeValue = convertStringToDouble(strTime.getText());
+        durValue = convertStringToDouble(dur.getText());
+        termValue = convertStringToDouble(term.getText());
+        freqValue = convertStringToDouble(freq.getText());
+        possValue = convertStringToDouble(poss.getText());
+        kwValue = convertStringToDouble(kw.getText());
+        jumpValue = convertStringToDouble(jump.getText());
+        System.out.println("AMP:"+ampValue);
+        System.out.println("================");
+    }
+
+    private double convertStringToDouble(String str) {
+        return Double.parseDouble(str.replaceAll("[^\\d]", ""));
     }
 
     public static void main(String[] args) {
