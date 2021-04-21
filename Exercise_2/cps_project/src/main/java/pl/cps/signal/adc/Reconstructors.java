559f1ec5fb90658cee08113684cb13dfff55324e
d-jakubik <224307@edu.p.lodz.pl>
2021-04-21 18:45:54 +0200

    Added zeroOrderInterpolation with test

--- Exercise_2/cps_project/src/main/java/pl/cps/signal/adc/Reconstructors.java
+++ Exercise_2/cps_project/src/main/java/pl/cps/signal/adc/Reconstructors.java
@@ -5,9 +5,47 @@ import pl.cps.signal.model.Data;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 public class Reconstructors {
+
+    public static double zeroOrderInterpolationForPoint(List<Data> data, double xValueToCalculateYValue) {
+        Optional<Data> lowerPoint = data.stream().filter(new Predicate<Data>() {
+                    @Override
+                    public boolean test(Data data) {
+                        if (data.getX() <= xValueToCalculateYValue) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }).max(Comparator.comparing(i -> i.getX())),
+                greaterPoint = data.stream().filter(new Predicate<Data>() {
+                    @Override
+                    public boolean test(Data data) {
+                        if (data.getX() >= xValueToCalculateYValue) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }).min(Comparator.comparing(i -> i.getX()));
+        if (lowerPoint.isEmpty()||greaterPoint.isEmpty()){
+            return 0;
+        }
+
+        return lowerPoint.get().getY();
+    }
+
+    public static List<Data> zeroOrderInterpolation(List<Data> data, int samplesPerSecond) {
+        List<Data> resultData = new ArrayList<Data>();
+        double maxX = data.stream().max(Comparator.comparing(i -> i.getX())).get().getX(),
+                minX = data.stream().min(Comparator.comparing(i -> i.getX())).get().getX();
+        for (double i = minX; i <= maxX; i += 1.0 / (double) samplesPerSecond) {
+            resultData.add(new Data(i, zeroOrderInterpolationForPoint(data, i)));
+        }
+        return resultData;
+    }
+
     public static List<Data> firstOrderInterpolation(List<Data> data, int samplesPerSecond) {
         List<Data> resultData = new ArrayList<Data>();
         double maxX = data.stream().max(Comparator.comparing(i -> i.getX())).get().getX(),
@@ -20,14 +58,14 @@ public class Reconstructors {
 
     public static double firstOrderInterpolationForPoint(List<Data> data, double xValueToCalculateYValue) {
         Data lowerPoint = data.stream().filter(new Predicate<Data>() {
-            @Override
-            public boolean test(Data data) {
-                if (data.getX() <= xValueToCalculateYValue) {
-                    return true;
-                }
-                return false;
-            }
-        }).max(Comparator.comparing(i -> i.getX())).get(),
+                    @Override
+                    public boolean test(Data data) {
+                        if (data.getX() <= xValueToCalculateYValue) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }).max(Comparator.comparing(i -> i.getX())).get(),
                 greaterPoint = data.stream().filter(new Predicate<Data>() {
                     @Override
                     public boolean test(Data data) {
--- Exercise_2/cps_project/src/test/java/pl/cps/signal/adc/ReconstructorsTest.java
+++ Exercise_2/cps_project/src/test/java/pl/cps/signal/adc/ReconstructorsTest.java
@@ -22,13 +22,42 @@ class ReconstructorsTest {
             assertEquals(-i / 10.0, Reconstructors.firstOrderInterpolationForPoint(minusData, -i / 10.0), 0.01);
         }
     }
+
     @Test
-    public void firstOrderInterpolationTest(){
+    public void firstOrderInterpolationTest() {
         List<Data> reconstructedData = Reconstructors.firstOrderInterpolation(
-                Arrays.asList(new Data(0.0, 0.0),new Data(1.0, 1.0)), 100);
+                Arrays.asList(new Data(0.0, 0.0), new Data(1.0, 1.0)), 100);
         for (int i = 0; i < 100; i++) {
-            assertEquals(i / 100.0, reconstructedData.get(i).getX(),0.0001);
-            assertEquals(i / 100.0, reconstructedData.get(i).getY(),0.0001);
+            assertEquals(i / 100.0, reconstructedData.get(i).getX(), 0.0001);
+            assertEquals(i / 100.0, reconstructedData.get(i).getY(), 0.0001);
         }
+    }
+
+    @Test
+    public void zeroOrderInterpolationForPointTest() {
+        List<Data> data = new ArrayList<Data>();
+        for (int i = 0; i < 11; i++) {
+            data.add(new Data((double) i, i % 3.0));
+        }
+        for (int i = 0; i < 110; i++) {
+            if (i <= 100) {
+                assertEquals(Math.floor((i / 10.0) % 3.0), Reconstructors.zeroOrderInterpolationForPoint(data, i / 10.0));
+            }
+            if (i > 100) {
+                assertEquals(0, Reconstructors.zeroOrderInterpolationForPoint(data, i / 10.0));
+            }
+        }
+
+    }
+
+    @Test
+    public void zeroOrderInterpolation() {
+        List<Data> data = new ArrayList<Data>(), resultData;
+        for (int i = 0; i < 11; i++) {
+            data.add(new Data((double) i, i % 3.0));
+        }
+        resultData = Reconstructors.zeroOrderInterpolation(data, 100);
+        resultData.stream().forEach(i ->
+                assertEquals(Math.floor(i.getX())%3.0,i.getY()));
     }
 }
